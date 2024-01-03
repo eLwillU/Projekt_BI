94c553063f0e5f0840ac90330655aa6f6f45ffac
AngryBacteria <AngryBacteria@protonmail.com>
2024-01-03 14:40:51 +0100

    update todo

--- shiny_dashboard/scripts/testing.R
+++ shiny_dashboard/scripts/testing.R
@@ -1,5 +1,13 @@
 source("scripts/preprocessing.R")
 source("scripts/models.R")
 library(caret)
+library(dplyr)
 
-df <- get_raw_clinical_data()
+df <- get_raw_data(balance_data = FALSE)
+
+summary(df$death_from_cancer)
+
+clinical <- get_raw_clinical_data()
+gene <- get_raw_gene_data()
+
+gene <- gene %>% select(-death_from_cancer)
