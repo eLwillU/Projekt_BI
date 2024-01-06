e8c783a5962736d44c66e605e04ca154a9243d1b
Dominic Willi <dominic.willi@students.bfh.ch>
2024-01-06 10:04:04 +0100

    small update

--- shiny_dashboard/app.R
+++ shiny_dashboard/app.R
@@ -8,18 +8,18 @@ ui <- dashboardPage(
   dashboardHeader(title = "Breast-Cancer Dashboard"),
   
   dashboardSidebar(sidebarMenu(
-    menuItem("Data Overview", tabName = "overview", icon = icon("house")),
+    menuItem("Data Overview", icon = icon("house"), tabName = "overview"),
     menuItem("Clinical Prognosis",tabName = "prognosis_clinical",icon = icon("person-dress"))
   )),
   
   dashboardBody(tabItems(
-    ### clinical prognosis
     tabItem(tabName = "prognosis_clinical",
       get_clinical_prognosis_ui()
     ),
     tabItem(tabName = "overview",
             get_overview_ui()
-  ))),
+  )
+  )),
 )
 
 
