ebc08d0213e3e844190d228ceb8e6b5780f6fc70
AngryBacteria <AngryBacteria@protonmail.com>
2024-01-07 20:07:35 +0100

    some more boxplots

--- shiny_dashboard/dashboards/overview.R
+++ shiny_dashboard/dashboards/overview.R
@@ -9,73 +9,73 @@ get_overview_ui <- function() {
       "selectedPlots",
       "Select Plots to Show",
       inline = TRUE,
-      choices = list(
-        "Boxplots" = "showBoxplots",
-        "Piecharts" = "showPiecharts"
-      ),
+      choices = list("Boxplots" = "showBoxplots",
+                     "Piecharts" = "showPiecharts"),
       selected = list("showBoxplots", "showPiecharts")
     ),
     
-    tabsetPanel(type = "tabs",
-                tabPanel(
-                  "General",
-                  fluidPage(
-                    h1("General"),
-                    conditionalPanel(
-                      condition = "input.selectedPlots.includes('showBoxplots')",
-                      box(plotlyOutput("cancerTypeBoxplot")),
-                      box(plotlyOutput("survivalBoxplot")),
-                      box(plotlyOutput("ageBoxplot")),
-                      box(plotlyOutput("tumorSizeBoxplot")),
-                    ),
-                    conditionalPanel(
-                      condition = "input.selectedPlots.includes('showPiecharts')",
-                      box(plotlyOutput("cancerTypePiechart")),
-                      box(plotlyOutput("surgeryTypePiechart")),
-                      box(plotlyOutput("chemoPiechart")),
-                      box(plotlyOutput("hormonePiechart")),
-                      box(plotlyOutput("cohortPiechart")),
-                    ),
-                  )
-                ),
-                tabPanel(
-                  "Heatmaps",
-                  fluidPage(
-                    h1("Heatmaps"),
-                    selectInput("showDendros", "Show Dendrograms",
-                                c(yes = "yes", no = "no")
-                    ),
-                    conditionalPanel(
-                      condition = "input.showDendros == 'yes'",
-                      checkboxInput("input.dendro", "Show dendrograms", F),
-                      box(plotOutput("heatmapDFC")),
-                      box(plotOutput("heatmapNoDFC")),
-                    ),
-                    conditionalPanel(
-                      # TODO: Plots without dendrogram
-                      condition = "input.showDendros == 'no'",
-                      box(plotlyOutput("plotlyHeatmapDFC")),
-                      box(plotlyOutput("plotlyHeatmapNoDFC")),
-                    ),
-                  )
-                ),
-                tabPanel(
-                  "PCA",
-                  fluidPage(
-                    h1("PCA Analysis"),
-                    # PCA plots
-                    box(plotOutput("pca1")),
-                    box(plotOutput("pca2")),
-                    box(plotOutput("pca3")),
-                    box(plotOutput("pca4")),
-                  )
-                ),
-                
-    )))
+    tabsetPanel(
+      type = "tabs",
+      tabPanel(
+        "General",
+        fluidPage(
+          h1("General"),
+          conditionalPanel(
+            condition = "input.selectedPlots.includes('showBoxplots')",
+            box(plotlyOutput("cancerTypeBoxplot")),
+            box(plotlyOutput("survivalBoxplot")),
+            box(plotlyOutput("ageBoxplot")),
+            box(plotlyOutput("lymphnodesBoxplot")),
+            box(plotlyOutput("mutationcountBoxplot")),
+            box(plotlyOutput("tumorSizeBoxplot")),
+          ),
+          conditionalPanel(
+            condition = "input.selectedPlots.includes('showPiecharts')",
+            box(plotlyOutput("cancerTypePiechart")),
+            box(plotlyOutput("surgeryTypePiechart")),
+            box(plotlyOutput("chemoPiechart")),
+            box(plotlyOutput("hormonePiechart")),
+            box(plotlyOutput("cohortPiechart")),
+          ),
+        )
+      ),
+      tabPanel(
+        "Heatmaps",
+        fluidPage(
+          h1("Heatmaps"),
+          selectInput("showDendros", "Show Dendrograms",
+                      c(yes = "yes", no = "no")),
+          conditionalPanel(
+            condition = "input.showDendros == 'yes'",
+            checkboxInput("input.dendro", "Show dendrograms", F),
+            box(plotOutput("heatmapDFC")),
+            box(plotOutput("heatmapNoDFC")),
+          ),
+          conditionalPanel(# TODO: Plots without dendrogram
+            condition = "input.showDendros == 'no'",
+            box(plotlyOutput(
+              "plotlyHeatmapDFC"
+            )),
+            box(plotlyOutput(
+              "plotlyHeatmapNoDFC"
+            )),),
+        )
+      ),
+      tabPanel("PCA",
+               fluidPage(
+                 h1("PCA Analysis"),
+                 # PCA plots
+                 box(plotOutput("pca1")),
+                 box(plotOutput("pca2")),
+                 box(plotOutput("pca3")),
+                 box(plotOutput("pca4")),
+               )),
+      
+    )
+  ))
 }
 
-get_overview_Server <- function(input, output){
-  
+get_overview_Server <- function(input, output) {
   # prep data
   clinical_data <- loadData("raw_clinical_data_unbalanced.RDS")
   all_data <- loadData("raw_data_unbalanced.RDS")
@@ -92,7 +92,7 @@ get_overview_Server <- function(input, output){
         clinical_data,
         clinical_data$cancer_type_detailed,
         clinical_data$overall_survival_months,
-        title = "Survival in months by cancer type",
+        title = "Months alive by cancer type",
         xaxis = "Cancer type",
         yaxis = "Survival in months"
       )
@@ -103,7 +103,7 @@ get_overview_Server <- function(input, output){
         clinical_data,
         clinical_data$death_from_cancer,
         clinical_data$overall_survival_months,
-        title = "Survival in months by survival from cancer",
+        title = "Months alive by death from cancer",
         xaxis = "Death from cancer",
         yaxis = "Survival in months"
       )
@@ -114,9 +114,31 @@ get_overview_Server <- function(input, output){
         clinical_data,
         clinical_data$death_from_cancer,
         clinical_data$age_at_diagnosis,
-        title = "Age at diagnosis",
+        title = "Age at diagnosis by death from cancer",
         xaxis = "Death from cancer",
         yaxis = "Age at diagnosis"
+      )
+    )
+  output$lymphnodesBoxplot <-
+    renderPlotly(
+      get_generic_boxplot(
+        clinical_data,
+        clinical_data$death_from_cancer,
+        clinical_data$lymph_nodes_examined_positive,
+        title = "Months alive by lymphnodes examined positive",
+        xaxis = "Death from cancer",
+        yaxis = "Lymphnodes tested positive"
+      )
+    )
+  output$mutationcountBoxplot <-
+    renderPlotly(
+      get_generic_boxplot(
+        clinical_data,
+        clinical_data$death_from_cancer,
+        clinical_data$mutation_count,
+        title = "Months alive by mutation count",
+        xaxis = "Death from cancer",
+        yaxis = "Mutation count"
       )
     )
   output$tumorSizeBoxplot <-
@@ -125,28 +147,73 @@ get_overview_Server <- function(input, output){
         clinical_data,
         clinical_data$death_from_cancer,
         clinical_data$tumor_size,
-        title = "Tumor Size by survival",
+        title = "Tumor size by death from cancer",
         xaxis = "Death from cancer",
         yaxis = "Tumor size"
       )
     )
   # Piecharts
-  output$cancerTypePiechart <- renderPlotly(get_generic_piechart(clinical_data, labels=clinical_data$cancer_type_detailed, "Cancer Distribution", labelType = "percent"))
-  output$surgeryTypePiechart <- renderPlotly(get_generic_piechart(clinical_data, labels=clinical_data$type_of_breast_surgery, "Surgery Distribution"))
-  output$chemoPiechart <- renderPlotly(get_generic_piechart(clinical_data, labels=clinical_data$chemotherapy, "Chemotherapy Distribution"))
-  output$hormonePiechart <- renderPlotly(get_generic_piechart(clinical_data, labels=clinical_data$hormone_therapy, "Hormone-Therapy Distribution"))
-  output$cohortPiechart <- renderPlotly(get_generic_piechart(clinical_data, labels=clinical_data$cohort, "Cohort Distribution"))
+  output$cancerTypePiechart <-
+    renderPlotly(
+      get_generic_piechart(
+        clinical_data,
+        labels = clinical_data$cancer_type_detailed,
+        "Cancer type Distribution",
+        labelType = "percent"
+      )
+    )
+  output$surgeryTypePiechart <-
+    renderPlotly(
+      get_generic_piechart(
+        clinical_data,
+        labels = clinical_data$type_of_breast_surgery,
+        "Surgery type Distribution"
+      )
+    )
+  output$chemoPiechart <-
+    renderPlotly(
+      get_generic_piechart(
+        clinical_data,
+        labels = clinical_data$chemotherapy,
+        "Chemotherapy distribution"
+      )
+    )
+  output$hormonePiechart <-
+    renderPlotly(
+      get_generic_piechart(
+        clinical_data,
+        labels = clinical_data$hormone_therapy,
+        "Hormone-Therapy distribution"
+      )
+    )
+  output$cohortPiechart <-
+    renderPlotly(
+      get_generic_piechart(clinical_data, labels = clinical_data$cohort, "Cohort Distribution")
+    )
   
   
   # Heatmaps
-  output$heatmapDFC <- renderPlot({get_static_heatmap(gene_df, death_from_cancer = TRUE)})
-  output$plotlyHeatmapDFC <- renderPlotly({ get_plotly_heatmap(gene_df, death_from_cancer = TRUE)})
-  output$heatmapNoDFC <- renderPlot({get_static_heatmap(gene_df, death_from_cancer = FALSE)})
-  output$plotlyHeatmapNoDFC <- renderPlotly({ get_plotly_heatmap(gene_df, death_from_cancer = FALSE)})
+  output$heatmapDFC <-
+    renderPlot({
+      get_static_heatmap(gene_df, death_from_cancer = TRUE)
+    })
+  output$plotlyHeatmapDFC <-
+    renderPlotly({
+      get_plotly_heatmap(gene_df, death_from_cancer = TRUE)
+    })
+  output$heatmapNoDFC <-
+    renderPlot({
+      get_static_heatmap(gene_df, death_from_cancer = FALSE)
+    })
+  output$plotlyHeatmapNoDFC <-
+    renderPlotly({
+      get_plotly_heatmap(gene_df, death_from_cancer = FALSE)
+    })
   
   # PCA plots
   output$pca1 <- renderPlot(get_pca_scree_all_numeric(all_data))
-  output$pca2 <- renderPlot(get_pca_scree_clinical_numeric(clinical_data))
+  output$pca2 <-
+    renderPlot(get_pca_scree_clinical_numeric(clinical_data))
   output$pca3 <- renderPlot(get_pca_scree_all_gene(gene_data))
   output$pca4 <- renderPlot(get_pca_scree_filtered_gene(gene_df))
   return(output)
